2638d9be18fe9837b94336204be1f95f2b3fdb45
k1 <kiyantsev90@mail.ru>
2024-06-15 02:18:41 +0300

    debug

--- main.js
+++ main.js
@@ -1,12 +1,10 @@
 function createCarousel({
-  width,
-  listSelector,
-  cardsToDisplay,
-  currentSelector,
-  leftArrowSelector,
-  rightArrowSelector,
-  changeCounter
-}) {
+                          width,
+                          listSelector,
+                          cardsToDisplay,
+                          currentSelector,
+                          changeCounter
+                        }) {
 
   const list = document.querySelector(listSelector);
   const listEl = list.children
@@ -17,51 +15,61 @@ function createCarousel({
 
   const current = document.querySelector(currentSelector)
 
-  changeCounter({currentElement:current, currentPage, cardsToDisplay})
-
+  changeCounter({currentElement: current, currentPage, cardsToDisplay})
 
   const transformCarousel = () => {
     list.style.transform = `translateX(${(currentPage - 1) * -width + 'px'})`;
-    changeCounter({currentElement:current, currentPage, cardsToDisplay})
+    changeCounter({currentElement: current, currentPage, cardsToDisplay})
   }
 
-  document.querySelector(leftArrowSelector).addEventListener('click', ()=> {
-    currentPage = Math.max(currentPage - 1, 1);
-    transformCarousel()
-  })
-
-  document.querySelector(rightArrowSelector).addEventListener('click', ()=> {
-    currentPage = Math.min(currentPage + 1, availablePages);
-    transformCarousel()
-  })
+  return {
+    getCurrentPage: () => currentPage,
+    nextPage: () => {
+      currentPage = (currentPage + availablePages) % availablePages + 1
+      transformCarousel()
+    },
+    prevPage: () => {
+      currentPage = (currentPage - 2 + availablePages) % availablePages + 1
+      transformCarousel()
+    }
+  }
 }
+
 const mediaQuery = window.matchMedia('(max-width: 1280px)')
 
-createCarousel({
+function setValueToCounter({currentElement, currentPage, cardsToDisplay}) {
+  currentElement.innerHTML = (currentPage + cardsToDisplay - 1).toString()
+}
+
+const tournament = createCarousel({
   width: mediaQuery.matches ? 447 : 450,
   listSelector: 'ul',
   cardsToDisplay: mediaQuery.matches ? 1 : 3,
   currentSelector: ".section-tournament__counter-current",
-  leftArrowSelector: ".section-tournament__carousel-button__left-arrow",
-  rightArrowSelector:'.section-tournament__carousel-button__right-arrow',
   changeCounter: setValueToCounter
 })
-createCarousel({
-  width: 355,
-  listSelector: '.grid-container',
-  cardsToDisplay: 1,
-  currentSelector: ".section-stages__carousel-container__choice",
-  leftArrowSelector: ".section-stages__carousel-button__left-arrow",
-  rightArrowSelector:'.section-stages__carousel-button__right-arrow',
-  changeCounter: highlightCircle
-})
-function setValueToCounter({currentElement, currentPage, cardsToDisplay}) {
-  currentElement.innerHTML = (currentPage + cardsToDisplay - 1).toString()
-}
-function highlightCircle({ currentPage }){
+
+document.querySelector(".section-tournament__carousel-button__left-arrow").addEventListener('click', tournament.prevPage)
+document.querySelector('.section-tournament__carousel-button__right-arrow').addEventListener('click', tournament.nextPage)
+
+setInterval(tournament.nextPage, 4000)
+
+function highlightCircle({currentPage}) {
   const circle = document.querySelector(`.carousel-circle:nth-child(${currentPage})`)
   document.querySelectorAll('.carousel-circle').forEach((circle) => circle.classList.remove('highlight'))
   circle.classList.add('highlight')
 }
+
+const stages = createCarousel({
+  width: 355,
+  listSelector: '.grid-container',
+  cardsToDisplay: 1,
+  currentSelector: ".section-stages__carousel-container__choice",
+  changeCounter: highlightCircle
+})
+
+document.querySelector(".section-stages__carousel-button__left-arrow").addEventListener('click', stages.prevPage)
+document.querySelector('.section-stages__carousel-button__right-arrow').addEventListener('click', stages.nextPage)
+
 
 
